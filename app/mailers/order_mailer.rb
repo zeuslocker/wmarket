05be3ecbe087f100ee105ec8c06b6621f126ea8a
zeuslocker <lapan40@ukr.net>
2017-05-29 21:59:45 +0300

    yandex mailer server

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,9 +1,8 @@
 class OrderMailer < ApplicationMailer
-  default from: "vova1997012@gmail.com"
   layout 'mailer'
   # OrderMailer.pay_info_email(Order.last).deliver_now
   def pay_info_email(order)
     @model = order
-    mail(to: 'lapan40@ukr.net', subject: 'Instructions')
+    mail(to: order.email, subject: 'Instructions')
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -7,10 +7,10 @@ Rails.application.initialize!
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
-:address        => 'smtp.gmail.com',
-:domain         => 'gmail.com',
-:port           => 587,
-:user_name      => 'vova1997012@gmail.com',
+:address        => 'smtp.yandex.ru',
+:domain         => 'yandex.ru',
+:port           => 465,
+:user_name      => 'lapan40@yandex.ru',
 :password       => 'zeusxlogan1715',
 :authentication => :plain,
 :enable_starttls_auto => true
